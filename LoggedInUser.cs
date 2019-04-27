14d354401227f2c48903aa6370408d5e1c5f5336
Your Name <you@example.com>
2019-04-27 12:57:23 +0300

    Add singelon LoggedUser

---
+++ LoggedInUser.cs
@@ -0,0 +1,53 @@
+﻿using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B19_Ex01_Matan_311116313_Moshe_305097453
+{
+
+    public sealed class LoggedInUser
+    {
+        private static readonly LoggedInUser instance = new LoggedInUser();
+        public User FacebookUser { get; private set; }
+        private LoginResult m_LoginResult;
+        private LoggedInUser()
+        {
+            LoginResult result = FacebookService.Login("2069724039730421", /// (desig patter's "Design Patterns Course App 2.4" app)
+
+            "public_profile",
+            "email",
+            "publish_to_groups",
+            "user_birthday",
+            "user_age_range",
+            "user_gender",
+            "user_link",
+            "user_tagged_places",
+            "user_videos",
+            "publish_to_groups",
+            "groups_access_member_info",
+            "user_friends",
+            "user_events",
+            "user_likes",
+            "user_location",
+            "user_photos",
+            "user_posts",
+            "user_hometown"
+          );
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                FacebookUser =result.LoggedInUser;
+            }
+        }
+       
+
+        public static LoggedInUser Instance()
+        {
+            return instance;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace B19_Ex01_Matan_311116313_Moshe_305097453
-{   // this is the program
+{
     static class Program
     {
         /// <summary>
@@ -17,36 +17,8 @@ namespace B19_Ex01_Matan_311116313_Moshe_305097453
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            LoginResult result = FacebookService.Login("2069724039730421", /// (desig patter's "Design Patterns Course App 2.4" app)
-
-            "public_profile",
-            "email",
-            "publish_to_groups",
-            "user_birthday",
-            "user_age_range",
-            "user_gender",
-            "user_link",
-            "user_tagged_places",
-            "user_videos",
-            "publish_to_groups",
-            "groups_access_member_info",
-            "user_friends",
-            "user_events",
-            "user_likes",
-            "user_location",
-            "user_photos",
-            "user_posts",
-            "user_hometown"
-          );
-
-            if (!string.IsNullOrEmpty(result.AccessToken))
-            {
-                Application.Run(new UserForm(result.LoggedInUser));
-            }
-            else
-            {
-                MessageBox.Show(result.ErrorMessage);
-            }         
+            LoggedInUser userInfo = LoggedInUser.Instance();
+            Application.Run(new UserForm(userInfo.FacebookUser));
         }
     }
 }
